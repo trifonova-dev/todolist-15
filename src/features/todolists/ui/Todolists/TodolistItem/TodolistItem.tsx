9583f0287d380083c7539d962f0dc24f6a3a1c50
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-21 16:38:45 +0700

    train

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistItem.tsx
@@ -20,7 +20,7 @@ export const TodolistItem = ({ todolist }: Props) => {
   return (
     <div>
       <TodolistTitle todolist={todolist} />
-      <CreateItemForm onCreateItem={createTask} />
+      <CreateItemForm disabled={todolist.entityStatus === "loading"} onCreateItem={createTask} />
       <Tasks todolist={todolist} />
       <FilterButtons todolist={todolist} />
     </div>
