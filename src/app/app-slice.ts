027c1697f543aabfbe03f32b7fe4f0ab6ea7c372
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-21 17:00:34 +0700

    train

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -27,7 +27,7 @@ export const appSlice = createSlice({
 })
 
 export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors
-export const { changeThemeModeAC, setAppStatusAC } = appSlice.actions
+export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC } = appSlice.actions
 export const appReducer = appSlice.reducer
 
 export type ThemeMode = "dark" | "light"
