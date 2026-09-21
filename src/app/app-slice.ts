2b4d5d3e6fb49b3059221d7d388c836688f28651
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-21 16:52:54 +0700

    train

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -6,10 +6,12 @@ export const appSlice = createSlice({
   initialState: {
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
+    error: null as string | null,
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectAppStatus: (state) => state.status,
+    selectAppError: (state) => state.error,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -18,10 +20,13 @@ export const appSlice = createSlice({
     setAppStatusAC: create.reducer<{ status: RequestStatus }>((state, action) => {
       state.status = action.payload.status
     }),
+    setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
+      state.error = action.payload.error
+    }),
   }),
 })
 
-export const { selectThemeMode, selectAppStatus } = appSlice.selectors
+export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors
 export const { changeThemeModeAC, setAppStatusAC } = appSlice.actions
 export const appReducer = appSlice.reducer
 
