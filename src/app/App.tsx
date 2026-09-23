1ce225f55c8823c93c3ec63eafe17566648cf286
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-23 23:01:17 +0700

    train

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
+import { ErrorSnackbar } from "@/common/components"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
@@ -18,6 +19,7 @@ export const App = () => {
         <CssBaseline />
         <Header />
         <Main />
+        <ErrorSnackbar />
       </div>
     </ThemeProvider>
   )
